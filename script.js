87bbcdf7d9d52955f702cc9ef60d7a5e841b5871
Labinot <godota400@hotmail.com>
2017-03-17 14:44:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,268 @@
+// Lecture Variables
+// var name = 'Labinot';
+// console.log(name);
+
+// var lastName = 'Jaha';
+// console.log(lastName);
+
+// var age = 24;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// -------------------// Variables 2 ------------------------------
+
+// var name = 'John';
+// var age = 26;
+
+// console.log(name + age);
+// console.log(age + age);
+
+// var job, isMarried;
+
+// console.log(job);
+
+// job = 'Teacher';
+// isMarried = false;
+
+// console.log(name + ' ka ' + age + ' vjet, dhe eshte ' + job + ' dhe statusi i ti martisor eshte?? ' + isMarried);
+// 
+// age = "tridhtegjasth";
+// job = "shofer";
+
+// console.log(name + ' ka ' + age + ' vjet, dhe eshte ' + job + ' dhe statusi i ti martisor eshte?? ' + isMarried);
+
+// var lastName = prompt('What is the last name ?');
+// console.log(lastName);
+
+
+// ----------------------- Operatrs  ----------------------------------------------
+
+// var now = 2016;
+// var birthYear = now - 26;
+
+// birthYear = now - 26 * 2;
+
+
+// console.log(birthYear);
+
+
+// ----------------------- if / else statement  ----------------------------------------------
+
+// var name = 'John';
+// var age = '26';
+// var isMarried = 'yes';
+
+// if (isMarried === 'yes') {
+// 	console.log(name + ' is married!');
+// } else {
+// 	console.log(name + ' is hopefolly going to marry soon!');
+// }
+
+// isMarried = false;
+
+// if (isMarried) {
+// 	console.log('E YES');
+// } else {
+// 	console.log('Nope');
+// }
+
+// ----------------------- boolean logic and switch ----------------------------------------------
+
+// var age = 25;
+
+// if ( age < 20 ) {
+// 	console.log('John is a teen');
+// } else if (age >= 20 && age <= 30) {
+// 	console.log('John is a young man');
+// } else {
+// 	console.log('John is a man');
+// }
+
+// var job = 'cop';
+
+// job = prompt('What does John do?');
+
+// switch (job) {
+// 	case 'teacher':
+// 	 console.log('Teaches kids');
+// 	 break;
+// 	case 'driver':
+// 	 console.log('John drives a car');
+// 	 break;
+// 	case 'cop':
+// 	 console.log('John helps people fight crime');
+// 	 break;
+
+// 	default:
+// 	 console.log('John does something else');
+// };
+
+// --------------------- The first Challange -------------------//
+
+// var johnHeight = 780;
+// var johnAge = 26;
+// var John = johnHeight + johnAge * 5;
+
+// var smithHeight = 375;
+// var smithAge = 28;
+// var Smith = smithHeight + smithAge * 5;
+
+// var kevinHeight = 200;
+// var kevinAge = 43;
+// var Kevin = kevinHeight + kevinAge * 5;
+
+// if (John > Smith && John > Kevin) {
+// 	console.log('John has ' + John + ' score!');
+// } else if (Kevin > Smith && Kevin > John) {
+//   console.log('Kevin has ' + Kevin + ' score!');
+// } else if (Smith > Kevin && Smith > John){
+// 	console.log('Smith has ' + Smith + ' score!'); 
+// } else {
+// 	console.log('theres a draw between')
+// };
+
+////// -------------------- Functions -------------------- //////
+
+// function calculateAge(yearsOfBirth){
+// 	var age = 2016 - yearsOfBirth;
+// 	return age;
+
+// }
+
+// var johnAge = calculateAge(1990);
+// console.log(johnAge);
+
+// function retirement(name, years) {
+	
+// }
+
+// retirement('John', 1992);
+
+
+
+////// -------------------- Arrays -------------------- //////
+/////////// 
+
+// var names = ['John', 'smith', 'ben'];
+
+// var years = new Array(1993, 1992, 1990);
+
+// console.log(names[0]);
+
+// names[1] = 'Luck';
+
+// console.log(names[1]);
+
+// var john = ['john', 'smith', 1990, false];
+
+
+
+// john.push('blue'); // Ending
+// john.unshift('Red'); //Begining
+// john.pop(); // removes the last element
+// john.shift(); // removes the first element
+
+// alert(john.indexOf('smith'));
+
+
+// console.log(john);
+
+////// -------------------- Objects -------------------- //////
+
+// console.log(john.lastname);
+// console.log(john['lastname']);
+
+// var zxc = 'job';
+// var red = 'status';
+
+// console.log(john[red]);
+
+// var jane = new Object();
+
+// jane.name = 'Jane';
+// jane.lastName = 'Smith';
+// jane['yearsOfBrith'] = 1969;
+// jane['job'] = 'retired';
+// jane['status'] = true;
+
+// console.log(jane);
+
+// var john = {
+// 	name: 'John',
+// 	lastname: 'Smith',
+// 	yearsOfBirth: 1990,
+// 	job: 'Teacher',
+// 	status: false,
+// 	family: ['Bob', 'Xeni', 'SHkelzeni'],
+// 	calculateAge: function(){
+// 		this.age = 2017 - this.yearsOfBirth;
+// 	}
+// }
+
+// john.calculateAge();
+// console.log(john);
+
+///////////////////////////////// LOOPS ////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
